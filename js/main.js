3b218b02c8d1449d3ddc6e3884e3c8b1aa8a0070
Adham-16 <a98347612z@gmail.com>
2024-12-10 02:32:53 +0200

    update

--- js/main.js
+++ js/main.js
@@ -2,71 +2,72 @@
 const mediaScreen = window.matchMedia("(max-width: 450px)")
 function openOrClose(x) {
     if (x.matches) {
-        $(".sidenav").animate({width : '105px'});
-        $(".text").animate({marginLeft: '105px'});
+        $(".sidenav").animate({ width: '105px' });
+        $(".text").animate({ marginLeft: '105px' });
     } else {
-        $(".sidenav").animate({width : '250px'});
-        $(".text").animate({marginLeft: '250px'});
+        $(".sidenav").animate({ width: '250px' });
+        $(".text").animate({ marginLeft: '250px' });
 
-    }}
-$(".sidebar").click(function () { 
+    }
+}
+$(".sidebar").click(function () {
     openOrClose(mediaScreen)
 })
-$(".closebtn").click(function (){
-    $(".sidenav").animate({width : '0'});
-    $(".text").animate({marginLeft: '0'});
+$(".closebtn").click(function () {
+    $(".sidenav").animate({ width: '0' });
+    $(".text").animate({ marginLeft: '0' });
 })
 
 $(".inner").slideUp();
 $(".inner:first").slideDown();
 $("document").ready(function () {
-    $(".Double").click(function () { 
-    $(this).next().slideToggle(500);
-    $(".inner").not( $(this).next() ).slideUp(500);
-});
+    $(".Double").click(function () {
+        $(this).next().slideToggle(500);
+        $(".inner").not($(this).next()).slideUp(500);
+    });
 })
 
 
 function countdownTimer() {
-	
-		var endAt = new Date("27 july 2024 9:56:00 GMT+01:00");			
-			endAt = (Date.parse(endAt) / 1000);
 
-			var timeNow = new Date();
-			timeNow = (Date.parse(timeNow) / 1000);
+    var endAt = new Date("10 March 2024 00:00:00 GMT+01:00");
 
-			var timeLeft = endAt - timeNow;
+    endAt = (Date.parse(endAt) / 1000);
 
-			var days = Math.floor(timeLeft / 86400); 
-			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+    var timeNow = new Date();
+    timeNow = (Date.parse(timeNow) / 1000);
 
-			if (hours < "10") { hours = "0" + hours; }
-			if (minutes < "10") { minutes = "0" + minutes; }
-			if (seconds < "10") { seconds = "0" + seconds; }
+    var timeLeft = endAt - timeNow;
 
-			$(".days").html(days + "<span>D</span>");
-			$(".hours").html(hours + "<span>H</span>");
-			$(".minutes").html(minutes + "<span>M</span>");
-			$(".seconds").html(seconds + "<span>S</span>");		
-	}
-	setInterval(function() { countdownTimer(); }, 1000);
+    var days = Math.floor(timeLeft / 86400);
+    var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+    var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+    var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
 
-    var max = 100;
-$('textarea').keyup(function() {
+    if (hours < "10") { hours = "0" + hours; }
+    if (minutes < "10") { minutes = "0" + minutes; }
+    if (seconds < "10") { seconds = "0" + seconds; }
+
+    $(".days").html(days + "<span>D</span>");
+    $(".hours").html(hours + "<span>H</span>");
+    $(".minutes").html(minutes + "<span>M</span>");
+    $(".seconds").html(seconds + "<span>S</span>");
+}
+setInterval(function () { countdownTimer(); }, 1000);
+
+var max = 100;
+$('textarea').keyup(function () {
     var length = $(this).val().length;
-    var numOfCher = max-length;
-    if(numOfCher<=0)
-            {
-                $(".characters").text("your available character finished");
-            }
-        else{
-                $(".characters").text(numOfCher);
-            }
+    var numOfCher = max - length;
+    if (numOfCher <= 0) {
+        $(".characters").text("your available character finished");
+    }
+    else {
+        $(".characters").text(numOfCher);
+    }
 });
 
-$(".btn").click(function () { 
+$(".btn").click(function () {
     console.log("hi");
     $('textarea').val("");
 
